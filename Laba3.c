1ca183908da365ec62acc0c33028ef2edb5f5b48
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-03-29 13:16:50 +0300

    Add files via upload

--- Laba3.c
+++ Laba3.c
@@ -1,19 +1,28 @@
-#include "stm32fxx.h"
+#include "stm32f2xx.h"                  // Device header
 
 void delay() {
 	unsigned long i = 0;
-	for (i = 0; i < 2000000; i++) {}
+	for (i = 0; i < 1000000; i++) {}
 }
 
 int main () {
-	RCC->AHB1ENR |= lul<<6;
-	RCC->AHB1ENR |= lul<<0;
-	GPIOG->MODER = (GPIOG->MODER & ~lul<<15) | lul<<14;
-	GPIOG->MODER = ()
+	RCC->AHB1ENR |= 1ul<<6; // PORTS ACTIVATION
+	RCC->AHB1ENR |= 1ul<<0;	
+	RCC->AHB1ENR |= 1ul<<7;
+		
+	GPIOH->MODER = (GPIOH->MODER & ~1ul<<15) | 1ul<<14;  // OUTPUT PORTS
+	GPIOH->MODER = (GPIOH->MODER & ~1ul<<13) | 1ul<<12;
+	GPIOH->MODER = (GPIOH->MODER & ~1ul<<7) | 1ul<<6;
+		
+	GPIOG->MODER = (GPIOG->MODER & ~1ul<<31) & ~1ul<<30;  // INPUT PORTS
+	GPIOA->MODER = (GPIOA->MODER & ~1ul<<1) & ~1ul<<0;
+	
 	for (;;) {
-		GPIOG->ODR |= lul<<7;
+		
+		GPIOH->ODR |= 1ul<<6;
 		delay();
-		GPIOG->ODR &= ~lul<<7;
+		GPIOH->ODR &= ~1ul<<6;
 		delay();
+	
 	}
 }
