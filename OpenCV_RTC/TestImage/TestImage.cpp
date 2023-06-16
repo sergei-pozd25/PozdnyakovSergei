8f3c573545f6d90a1f74005546cb58ce47d20c73
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-06-16 13:12:31 +0300

    Add files via upload

---
+++ OpenCV_RTC/TestImage/TestImage.cpp
@@ -0,0 +1,19 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+//using namespace cv;
+
+int main(int argc, char** argv) {
+    cv::Mat test_image = cv::imread("/home/sergei/opencv_build/opencv/Image1/OpenCV.jpg", cv::IMREAD_UNCHANGED);
+    //cv::namedWindow("Display Image");
+    cv::imshow ("Image", test_image);
+
+    cv::waitKey(0);
+
+    cout << "ImageTest" << endl;
+    return 0;
+}
