541598b3519e7f00a8bbdfca549e12b5a7cf0648
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-06-16 15:16:18 +0300

    Add files via upload

--- OpenCV_RTC/Hello/HelloOpenCV.cpp
+++ OpenCV_RTC/Hello/HelloOpenCV.cpp
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 #include <opencv4/opencv2/opencv.hpp>
-
+using namespace std;
 
 int main(){
-    printf("Hello, OpenCV\n");
+    cout << "Hello, OpenCV" << endl;
     return 0;
 }
