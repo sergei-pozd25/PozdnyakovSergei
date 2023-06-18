3c277766ead649d276bc9f903bc21e19510b5c9a
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-06-18 22:33:30 +0300

    Update VideoTest.cpp

--- OpenCV_RTC/TestVideo/VideoTest.cpp
+++ OpenCV_RTC/TestVideo/VideoTest.cpp
@@ -4,24 +4,26 @@
 #include <opencv4/opencv2/core/core.hpp>
 #include <iostream>
 #include <stdio.h>
-using namespace std;
-//using namespace cv;
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
 
-    cv::Mat frame;
-    cv::VideoCapture capture;
-    capture.open("");
-    for (;;) {
-        capture>>frame;
-        if (frame.empty()) break;
-        cv::imshow("Window", frame);
-        if (cv::waitKey(30)>=0) break;
-    }
+    Mat frame;
+    VideoCapture capture;
+    capture.open(0);
 
-    cout << "Test Video" << endl;
+    while (1) {
+        capture >> frame;
+
+        if (frame.empty()) break;
+
+        imshow("W", frame);
+
+        if (waitKey(30) >= 0) break;
+        //click escape button to turn video off
+    }
+    cout << "Video test" << endl;
     return 0;
 }
