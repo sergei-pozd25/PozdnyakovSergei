ac3f0c553ebdf5cbdcf99ba57bc16195a6ee9739
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-06-19 12:21:15 +0300

    Add files via upload

---
+++ OpenCV_RTC/WebcameraVideo/WebCam.cpp
@@ -0,0 +1,43 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/core/core.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+
+class Camera_Exceptions: public std::domain_error {
+public:
+    Camera_Exceptions(const char* const error) : std::domain_error(error) {}
+};
+
+Camera_Exceptions NO_CAMERA ("Сamera with this index is not connected");
+
+
+int main(int argc, char** argv) {
+    Mat video;
+    string cam_name;
+    VideoCapture capture;
+    unsigned int index;
+    cin >> index;
+    capture.open(index);
+
+    switch (index) {
+        case 0: cam_name = "Laptop webcam";
+            break;
+        case 1 ... 9: cam_name = "Not built-in camera";
+            break; }
+
+    while (1) {
+        capture >> video;
+        if (video.empty()) break;
+        resize(video, video, Size(), 0.5, 0.5, INTER_NEAREST);
+        // resize output video
+        imshow(cam_name, video);
+        // output video on screen
+        if (waitKey(30) >= 0) break;  //click escape button to turn video off
+    }
+    cout << cam_name << endl;
+    return 0;
+}
